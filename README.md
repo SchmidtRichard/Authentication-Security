8fa72633c33bae57d3176d515aa5a84abdb6e3d5
SchmidtRichard <ri.schmidta@gmail.com>
2021-04-14 15:46:38 +0100

    Level 1 Security - Register Users with Username and Password, Install mongoose, setup POST routes for register and login and render to the pages

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # Secrets Project
+
+* * *
+
+# [Express](https://expressjs.com/en/starter/installing.html)
+
+Use the npm init command to create a `package.json` file for your application.
+
+```express
+npm init -y
+```
+
+Install some packages(`express`, `ejs`, `body-parser` and `mongoose`)
+
+```express
+npm i express ejs body-parser mongoose
+```
+
+# Security Level 1 - The Lowest Level
+
+## Simply creating an account for the user to store the email & password into mongoDB (users collection)
+
+### HTTP POST Request/POST Route Code Example
+
+#### POST Request to Register Route
+
+```js
+//POST request (register route) to post the username and password the user enter when registering
+app.post("/register", function(req, res) {
+  //Create the new user using the User model
+  const newUser = new User({
+    //Values from the userSchema checked against the register.ejs variables
+    email: req.body.username,
+    password: req.body.password
+  });
+  //Save the new user
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      /_
+      Only render the secrets page if the user is logged in
+      that is why there is no app.get("/secrets")... route
+      _/
+      res.render("secrets");
+    }
+  });
+});
+```
+
+#### POST Request to Login Route
+
+```js
+//POST request (login route) to login the user
+app.post("/login", function(req, res) {
+  //Check in mongoDB if the credentials entered exist in the DB
+  const username = req.body.username;
+  const password = req.body.password;
+
+  /_
+  Check the details entered above (username & password)
+  if the details exist in the DB and match what is in the DB
+  Look through the collection of Users (User)
+  _/
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      /_
+      If the user has been found in the DB
+      Check if the password is correct, if correct render to the secrets page
+      _/
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+```
--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-//const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
 //Create a new app instance using express
 const app = express();
@@ -20,6 +20,22 @@ app.use(bodyParser.urlencoded({
 //Tell the app to use all the statics files inside the public folder
 app.use(express.static("public"));
 
+//Connect to mongoDB
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//Setup the new userDB
+//Create the userSchema
+const userSchema = {
+  email: String,
+  password: String
+};
+
+//Setup a new User model and specify the name of the collection User
+const User = new mongoose.model("User", userSchema);
+
 //Add some GETs to view the EJS files/websites
 //Target the home/root route to render the home page
 app.get("/", function(req, res) {
@@ -34,6 +50,58 @@ app.get("/login", function(req, res) {
 //Target the register route to render the register page
 app.get("/register", function(req, res) {
   res.render("register");
+});
+
+//POST request (register route) to post the username and password the user enter when registering
+app.post("/register", function(req, res) {
+  //Create the new user using the User model
+  const newUser = new User({
+    //Values from the userSchema checked against the register.ejs variables
+    email: req.body.username,
+    password: req.body.password
+  });
+  //Save the new user
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      /*
+      Only render the secrets page if the user is logged in
+      that is why there is no app.get("/secrets")... route
+      */
+      res.render("secrets");
+    }
+  });
+});
+
+//POST request (login route) to login the user
+app.post("/login", function(req, res) {
+  //Check in mongoDB if the credentials entered exist in the DB
+  const username = req.body.username;
+  const password = req.body.password;
+
+  /*
+  Check the details entered above (username & password)
+  if the details exist in the DB and match what is in the DB
+  Look through the collection of Users (User)
+  */
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      /*
+      If the user has been found in the DB
+      Check if the password is correct, if correct render to the secrets page
+      */
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
 });
 
 
