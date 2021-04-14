1390fccdefaf30839a22ce5f86c71ec38a2cf92b
SchmidtRichard <ri.schmidta@gmail.com>
2021-04-14 12:05:17 +0100

    Install express ejs and body-parser packages and set up the server

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 //jshint esversion:6
+
+//Create 4 constants to require packages/modules
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+//const mongoose = require("mongoose");
+
+//Create a new app instance using express
+const app = express();
+
+//Tell the app to use EJS as its view engine as the templating engine
+app.set("view engine", "ejs");
+
+//Require body-parser module to parser the requests
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+//Tell the app to use all the statics files inside the public folder
+app.use(express.static("public"));
+
+//Set up the server to listen to port 3000
+app.listen(3000, function() {
+  console.log("Server started on port 3000!!!");
+});
